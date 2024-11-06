f6f331e183e06ad17eac2019cf8c776e4bde1212
Michael Mueller <michaelmuelleronline@gmx.de>
2024-11-06 21:18:02 +0100

    added functionality in AbstractTest

--- AbstractTest.py
+++ AbstractTest.py
@@ -1,11 +1,21 @@
+import logging
 from mi_py_test_lib.Test import Test
 
 class AbstractTest (Test):
-    
+    def __init__(self) -> None:
+        super().__init__()
+        
     async def exec(self) -> None:        
         await self._exec()
     
     async def _exec(self):
         raise NotImplementedError()
     
+    def _assert( self, condition:bool, assertion_description:str ):
+        if not condition:
+            raise AssertionError( f'{self.__class__.__name__} - Assertion FAILED: {assertion_description}' )
     
+    def _check( self, condition:bool, assertion_description:str ):
+        if not condition:
+            logging.warning( f'{self.__class__.__name__} - Check FAILED: {assertion_description}' )
+        
