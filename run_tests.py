d983f9f43ab4b49f992dc3573a81ab6d69735210
Michael Mueller <michaelmuelleronline@gmx.de>
2024-11-06 21:42:22 +0100

    finer work

---
+++ run_tests.py
@@ -0,0 +1,12 @@
+# built-in
+import os
+import sys
+import asyncio
+# local
+from mi_py_test.SelfTest import SelfTest
+
+async def main():
+    await SelfTest().exec()
+        
+if __name__ == "__main__":
+    asyncio.run(main())
